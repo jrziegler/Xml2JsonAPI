bf679135842f6305fbcbb35b32d54b43b35d6b1c
carlosziegler <carlos.ziegler@gmail.com>
2020-12-18 21:35:45 +0100

    normalize data

--- src/controllers/FIle/utils/convertToJson.ts
+++ src/controllers/FIle/utils/convertToJson.ts
@@ -1,7 +1,26 @@
-import xmlParser from "xml2js";
+import xmlParser, { processors } from "xml2js";
 import fs from "fs";
 
 export async function convertToJson(path: string) {
   const wpSampleXML = fs.readFileSync(path, "utf8");
-  return await xmlParser.parseStringPromise(wpSampleXML);
+
+  return await xmlParser.parseStringPromise(wpSampleXML, {
+    tagNameProcessors: [nameToLowerCase],
+    attrNameProcessors: [nameToLowerCase],
+    attrValueProcessors: [
+      nameToLowerCase,
+      processors.parseBooleans,
+      processors.parseNumbers,
+    ],
+    valueProcessors: [
+      nameToLowerCase,
+      processors.parseBooleans,
+      processors.parseNumbers,
+    ],
+    emptyTag: null,
+    explicitArray: false,
+  });
+}
+function nameToLowerCase(name: string) {
+  return name.toLowerCase();
 }
